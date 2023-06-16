1ae249b62c16ba10389d08422dc12cb79a50edc3
arif wijanarko <arifpethod@gmail.com>
2023-06-16 09:11:32 +0700

    tugas javascript 15
    untuk niomic

---
+++ tugas15.js
@@ -0,0 +1,13 @@
+function sisiMiring() {
+    // rumus : a2 + b2 = c2;
+    var ab = 8;
+    var bc = 6;
+    var ac = Math.sqrt(Math.pow(ab, 2) + Math.pow(bc, 2));
+
+    console.log("nilai ab :", ab);
+    console.log("nilai bc :", bc);
+    console.log("panjang sisi Ac pada segitiga siku-siku tersebut adalah : ",ac ,"cm");
+
+}
+
+sisiMiring();
